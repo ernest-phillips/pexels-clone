8b0e5e140337269edf46b174e0c189a8688e68ca
Ernest <ernestlphillips@gmail.com>
2021-07-14 16:44:37 -0700

    Created environment variable for secret

--- pexels-react/src/App.js
+++ pexels-react/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
+
+const API_KEY= process.env.REACT_APP_API_KEY
 class App extends Component {
   constructor(props) {
     super(props);
@@ -8,12 +10,12 @@ class App extends Component {
       isLoaded: false,
     };
   }
-
+  
   componentDidMount() {
     let myHeaders = new Headers();
     myHeaders.append(
       "Authorization",
-      "***REMOVED***"
+      API_KEY
     );
 
     let requestOptions = {
@@ -21,20 +23,24 @@ class App extends Component {
       headers: myHeaders,
       redirect: "follow",
     };
-    fetch("https://api.pexels.com/v1/curated?per_page=10", requestOptions)
+    fetch(
+      "https://api.pexels.com/v1/curated?page=2&per_page=14",
+      requestOptions
+    )
       .then((response) => response.json())
       .then((json) => {
         this.setState({
           isLoaded: true,
           photos: json.photos,
+          result: json.result,
         });
       })
-      .then((result) => console.log(result))
+      .then((response) => console.log(response))
       .catch((error) => console.log("error", error));
   }
 
   render() {
-    let { isLoaded, photos } = this.state;
+    let { isLoaded, photos, result } = this.state;
 
     if (!isLoaded) {
       return <div>Loading...</div>;
@@ -42,18 +48,22 @@ class App extends Component {
       return (
         <div className="App">
           <div className="photo-grid">
-            {photos.map((photo) => (
-              <div className="photos-column">
-                <a href={photo.src.large2x}>
-                  <li key={photo.id} className="photo">
+            <ul>
+              {photos.map((photo) => (
+                <li key={photo.id} className="photo">
+                  <a href={photo.src.large2x}>
                     <img
                       src={photo.src.medium}
                       alt={"Photo by " + photo.photographer}
                     ></img>
-                  </li>
-                </a>
-              </div>
-            ))}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <button onClick="">{console.log("My Button")}</button>
           </div>
         </div>
       );
